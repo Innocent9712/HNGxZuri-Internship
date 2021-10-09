f608459a7b74fc87ff11f0ea22662736b32875d5
Chukwuemeka Innocent <chukwuemeka140@gmail.com>
2021-10-09 10:12:54 +0100

    changes made.

--- displayName.js
+++
@@ -1,8 +0,0 @@
-//Program to display my Name
-
-let userFirstName = 'Innocent';
-let userLastName = "Chukwuemeka";
-
-function displayName (firstName, lastName) {
-    console.log(`My name is ${firstName} ${lastName}`)
-} displayName(userFirstName, userLastName)
